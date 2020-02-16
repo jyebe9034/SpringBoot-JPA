eec978a133a57f962a1ef4d42de822cc5bdc97c2
jihye <jyebe9034@gmail.com>
2020-02-16 23:52:24 +0900

    add P6Spy and finish test module

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,6 +17,7 @@ class MemberRepositoryTest {
 
     @Test
     @Transactional
+    @Rollback(false) // 기본적으로 테스트때는 데이터를 넣었다가 롤백시키는데 직접 데이터를 보고싶으면 이 설정을 넣어주면 됨.
     public void testMember() throws Exception {
         // given
         Member member = new Member();
@@ -28,6 +30,8 @@ class MemberRepositoryTest {
         // then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member); // JPA 엔티티 동일성 보장
+        System.out.println("findMember == member : " + (findMember == member)); // true
 
     }
 }
